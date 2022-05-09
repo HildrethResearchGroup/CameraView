357cc5858bf18827f0249405c253a03e7fe099e9
seeaya <connorbarnesdenver@icloud.com>
2022-05-09 10:47:19 -0600

    Initial Commit

---
+++ DNVNew/DNVNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNVNewApp.swift
+//  DNVNew
+//
+//  Created by Connor Barnes on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DNVNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
